cfaae3f829a2eba471fb237e1eb519862e62fa44
aizhanti <a.t.imankulova@gmail.com>
2020-04-21 12:34:23 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,36 @@
 const pokeData = require("./data");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 const setupServer = () => {
   /**
    * Create, set up and return your express server, split things into separate files if it becomes too long!
    */
+  return app;
 };
+
+app.get("/api/pokemon", async (request, response) => {
+  const limit = request.query.limit;
+  response.send(pokeData.pokemon.slice(0, limit));
+});
+
+app.post("/api/pokemon", async (request, response) => {
+  pokeData.pokemon.push(request.body);
+  response.send(pokeData.pokemon);
+});
+
+app.get("/api/pokemon/:key", async (request, response) => {
+  const key = request.params.key;
+  pokeData.pokemon.forEach((pok) => {
+    if (
+      Number(pok.id) === Number(key) ||
+      pok.name.toLowerCase() === key.toLowerCase()
+    ) {
+      response.send(pok);
+    }
+  });
+});
 
 module.exports = { setupServer };
