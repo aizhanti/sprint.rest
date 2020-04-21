60c2c7ce910921b772f10b3f0d285edc427d990b
aizhanti <a.t.imankulova@gmail.com>
2020-04-21 12:35:17 +0900

    Update _pokemonApi.js

--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -2,6 +2,8 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const { setupServer } = require("../src/server");
+chai.should();
+const expect = chai.expect;
 
 /*
  * This sprint you will have to create all tests yourself, TDD style.
@@ -13,5 +15,40 @@ describe("Pokemon API Server", () => {
   let request;
   beforeEach(() => {
     request = chai.request(server);
+  });
+
+  describe("GET /api/pokemon", () => {
+    it("should return json for full list pokemon", async () => {
+      const res = await request.get("/api/pokemon").query({ limit: 5 });
+      res.should.be.json;
+      JSON.parse(res.text).length.should.equal(5);
+    });
+  });
+
+  describe("POST /api/pokemon", () => {
+    it("should add a pokemon", async () => {
+      const newPok = {
+        id: "1512",
+        name: "Aizhan",
+      };
+      const res = await request.post("/api/pokemon").send(newPok);
+      JSON.parse(res.text).length.should.equal(152);
+    });
+  });
+
+  describe("GET /api/pokemon/:id", () => {
+    it("should return the Pokemon with the given id", async () => {
+      const res = await request.get("/api/pokemon/042");
+      res.should.be.json;
+      expect(res.body.name).to.equal("Golbat");
+    });
+  });
+
+  describe("GET /api/pokemon/:name", () => {
+    it("should return the Pokemon with given name", async () => {
+      const res = await request.get("/api/pokemon/Mew");
+      res.should.be.json;
+      expect(res.body.name).to.equal("Mew");
+    });
   });
 });
