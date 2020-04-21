5d15a99702e4837c318b321bb1437b103a36d376
aizhanti <a.t.imankulova@gmail.com>
2020-04-21 14:03:07 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -51,7 +51,7 @@ app.patch("/api/pokemon/:key", async (request, response) => {
 
 app.delete("/api/pokemon/:key", async (request, response) => {
   const key = request.params.key;
-  for (i = 0; i < pokeData.pokemon.length; i++) {
+  for (let i = 0; i < pokeData.pokemon.length; i++) {
     if (
       Number(pokeData.pokemon[i].id) === Number(key) ||
       pokeData.pokemon[i].name.toLowerCase() === key.toLowerCase()
@@ -60,6 +60,54 @@ app.delete("/api/pokemon/:key", async (request, response) => {
     }
   }
   response.send(pokeData.pokemon);
+  response.status(200).end();
+});
+
+app.get("/api/pokemon/:key/evolutions", async (request, response) => {
+  const key = request.params.key;
+  pokeData.pokemon.forEach((pok) => {
+    if (
+      Number(pok.id) === Number(key) ||
+      pok.name.toLowerCase() === key.toLowerCase()
+    ) {
+      response.send(pok.evolutions);
+    }
+  });
+  response.status(200).end();
+});
+
+app.get("/api/pokemon/:key/evolutions/previous", async (request, response) => {
+  const key = request.params.key;
+  pokeData.pokemon.forEach((pok) => {
+    if (
+      Number(pok.id) === Number(key) ||
+      pok.name.toLowerCase() === key.toLowerCase()
+    ) {
+      response.send(pok["Previous evolution(s)"]);
+    }
+  });
+  response.status(200).end();
+});
+
+app.get("/api/types", async (request, response) => {
+  const limit = request.query.limit;
+  response.send(pokeData.types.slice(0, limit));
+  response.status(200).end();
+});
+
+app.post("/api/types", async (request, response) => {
+  pokeData.types.push(request.body.newType);
+  response.send(pokeData.types);
+});
+
+app.delete("/api/types/:name", async (request, response) => {
+  const name = request.params.name;
+  for (let i = 0; i < pokeData.types.length; i++) {
+    if (pokeData.types[i] === name) {
+      pokeData.types.splice(i, 1);
+    }
+  }
+  response.send(pokeData.types);
   response.status(200).end();
 });
 
